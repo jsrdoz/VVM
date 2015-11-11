b919fb33145626cbc52756876c4005d54eac211b
jsrdoz <jsrdoz@gmail.com>
2015-11-11 14:42:22 +0800

    complete run

--- RUN/run.sh
+++ RUN/run.sh
@@ -7,7 +7,7 @@
 
 source $MODULESHOME/init/sh
 module purge
-module load intel14/comp intel14/hdf5-1.8.12 intel14/openmpi-1.8 intel14/netcdf-4.3.2 intel14/petsc-3.5.0 intel14/pnetcdf-1.3.1
+module load intel14/comp intel14/hdf5-1.8.12 intel14/openmpi-1.8 intel14/netcdf-4.3.2 intel14/petsc-3.6.2 intel14/pnetcdf-1.3.1
 
 cd $PBS_O_WORKDIR
 
