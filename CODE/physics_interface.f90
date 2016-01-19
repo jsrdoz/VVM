c65d739f1130bec55706fdfead610cfbe05fe91c
Heavenas <jsrdoz@gmail.com>
2016-01-19 22:52:14 +0800

    add a new microphyisics scheme add a new SST pattern code

--- CODE/physics_interface.f90
+++ CODE/physics_interface.f90
@@ -67,7 +67,8 @@
           PIBAR, &  ! Non-dimensional pressure at model layers
           RHO, &    ! Density at model layers
           THBAR, &  ! Basic state potential temperature profile at model layers
-          THBARZ    ! Basic state potential temperature profile at model interfaces
+          THBARZ    ! Basic state potential temperature profile at model
+interfaces
 
       LOGICAL (KIND=log_kind), INTENT(IN) :: &
           NOTHERM
@@ -161,7 +162,8 @@
       p0(0)  = PBAR(1)
 
       DO 20 K = 2, NK2
-        pi0(K-1)= PIBAR(K) + (PIBAR(K+1)-PIBAR(K)) / (ZT(K+1)-ZT(K)) * (ZZ(K)-ZT(K))
+        pi0(K-1)= PIBAR(K) + (PIBAR(K+1)-PIBAR(K)) / (ZT(K+1)-ZT(K)) *
+(ZZ(K)-ZT(K))
         p0(K-1) = PSFC * pi0(K-1) ** ( CP / RDRYA )
    20 CONTINUE
 
@@ -373,8 +375,10 @@
 !          print *,'qi ',qi3d(i,j,k), qi(i,j,k-1)
 !          stop
 !        endif
-        qb4 = QV3D(i,j,k) + QC3D(i,j,k) +QI3D(i,j,k) +QS3D(i,j,k) +QG3D(i,j,k) +QR3D(i,j,k) 
-        qafter = QV(i,k-1) + QC(i,k-1) +QI(i,k-1) +QS(i,k-1) +QG(i,k-1) +QR(i,k-1) 
+        qb4 = QV3D(i,j,k) + QC3D(i,j,k) +QI3D(i,j,k) +QS3D(i,j,k) +QG3D(i,j,k)
++QR3D(i,j,k) 
+        qafter = QV(i,k-1) + QC(i,k-1) +QI(i,k-1) +QS(i,k-1) +QG(i,k-1)
++QR(i,k-1) 
         if(abs(qafter-qb4)/qb4 > 1.e-10_dbl_kind) then
           print *,'t ',th3d(i,j,k)* pil0(K-1), theta(i,k-1)* pil0(K-1)
           print *,'q ',qb4, qafter
@@ -453,4 +457,3 @@
 
       RETURN
       END SUBROUTINE physics_interface
-      
